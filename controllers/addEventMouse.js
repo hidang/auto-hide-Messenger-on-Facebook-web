82ec4774fd1d9eb4e9b28ebc4dcaeeb1cd0975e1
hidang <yellowstudio.vn@gmail.com>
2020-12-31 23:58:52 +0700

    fix bug and goodbye 2020 and happy new year 2021

--- controllers/addEventMouse.js
+++ controllers/addEventMouse.js
@@ -1,17 +1,23 @@
 console.log('addEventMouse.js');
 const port = chrome.runtime.connect();
-var ChatTabs, I_ChatTabs, Chat_Tabs, I_Chat_Tabs, array_ChatTabName = [];
-var arrayChatTabName = [];
+var ChatTabs, I_ChatTabs, Chat_Tabs, I_Chat_Tabs, array_ChatTabName = [], 
+    arrayChatTabName = [], flag = false, ChatTabNames;
 //---------------------------------------------------------------------------------------
 window.setInterval(function(){ // Set interval for checking 😑😐
   I_ChatTabs = document.querySelectorAll('[data-testid="messenger-chat-title-text"]');
-  I_ChatTabs = [...I_ChatTabs];
   if (I_ChatTabs.length === 0) {
+    if(flag){
+      //console.log('just one');
+      localStorage.setItem('Chat_Tabs', '[]');
+      port.postMessage({ChatTabName: '[]'});
+    }
+    flag = false;
     return;
   }
+  flag = true;
   I_Chat_Tabs = JSON.parse(localStorage.getItem('Chat_Tabs'));
-  if (I_Chat_Tabs === "[]" || I_Chat_Tabs === null) {
-    console.log('dayne')
+  if (I_Chat_Tabs === "[]" || I_Chat_Tabs === null) {//trường hợp I_ChatTab.length >0 và I_Chat_Tabs chưa được set Item
+    //console.log('dayne')
     Add();
     return;
   }
@@ -19,31 +25,49 @@ window.setInterval(function(){ // Set interval for checking 😑😐
   I_ChatTabs.forEach(element => {
     arrayChatTabName.push(element.textContent);
   });
+
   arrayChatTabName.forEach(element => {
     //console.log(I_Chat_Tabs)
     if (!I_Chat_Tabs.includes(element)) {
-      Add();
+      Add(element);
     }
   });
+  I_Chat_Tabs.forEach(element => {
+    //console.log(I_Chat_Tabs)
+    if (!arrayChatTabName.includes(element)) {
+      Add(element);
+    }
+  });
+
 }, 500);
 //----------------------------------------------------------------------------------------
 
-function Add() {
+function Add(ChatTabName_New = true) {
+  console.log('add() ne');
   ChatTabs = document.querySelectorAll('[data-pagelet="ChatTab"]');
-  ChatTabs = [...ChatTabs];
   array_ChatTabName = [];
   if(ChatTabs !== "[]"){
     ChatTabs.forEach(element => {
-      var ChatTabNames = element.querySelectorAll('[data-testid="messenger-chat-title-text"]');
-      ChatTabNames = [...ChatTabNames];
+      ChatTabNames = element.querySelectorAll('[data-testid="messenger-chat-title-text"]');
       array_ChatTabName.push(ChatTabNames[0].textContent);//FIXME: trường hợp 2 tên giống nhau
       //console.log(ChatTabNames[0].textContent)
-      element.addEventListener('mouseenter', () => {//FIXME: check ton tai addEventListener
-        port.postMessage({thaotac: "show"});
-      });
-      element.addEventListener('mouseleave', () => {
-        port.postMessage({thaotac: "hide"});
-      });
+      if (ChatTabName_New === true) {//FIXME: check ton tai addEventListener
+        element.addEventListener('mouseenter', () => {
+          port.postMessage({thaotac: "show"});
+        });
+        element.addEventListener('mouseleave', () => {
+          port.postMessage({thaotac: "hide"});
+        });
+      }else{
+        if (ChatTabNames[0].textContent == ChatTabName_New) {
+          element.addEventListener('mouseenter', () => {
+            port.postMessage({thaotac: "show"});
+          });
+          element.addEventListener('mouseleave', () => {
+            port.postMessage({thaotac: "hide"});
+          });
+        }
+      }
     });
     //console.log(array_ChatTabName)
     port.postMessage({ChatTabName: JSON.stringify(array_ChatTabName)});
--- controllers/show_hide.js
+++ controllers/show_hide.js
@@ -2,19 +2,25 @@
 //[data-testid="messenger_incoming_text_row"]
 //[data-testid="mwchat_outgoing_row"]
 console.log('show_hide.js');
-
+var thaotac, sh_ChatTabs, elements1, elements2, sh_ChatTabNames;
 chrome.storage.local.get(["thaotac"], function(items) {
-  var thaotac = (items["thaotac"] || "");
+  thaotac = (items["thaotac"] || "");
   Show_Hide(thaotac);
 });
 function Show_Hide(thaotac){
+  sh_ChatTabs = document.querySelectorAll('[data-pagelet="ChatTab"]');
+  sh_ChatTabs.forEach(chattab => {
+    sh_ChatTabNames = chattab.querySelectorAll('[data-testid="messenger-chat-title-text"]');
+    //sh_ChatTabNames[0].textContent//FIXME: trường hợp 2 tên giống nhau
+    
+    elements1 = chattab.querySelectorAll('[data-testid="incoming_group"');//sender
+    elements1.forEach(element => {
+      element.style.visibility = thaotac;
+    });
+    elements2 = chattab.querySelectorAll('[data-testid="outgoing_group"');
+    elements2.forEach(element => {
+      element.style.visibility = thaotac;
+    });
+  });
 
-  
-  var elements = document.querySelectorAll('[data-testid="incoming_group"');//sender
-  for (let element of elements) 
-    element.style.visibility = thaotac;
-
-  var elements = document.querySelectorAll('[data-testid="outgoing_group"');
-  for (let element of elements) 
-    element.style.visibility = thaotac;
 }
