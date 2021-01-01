b9b0a7a3271da399fb4c84f5dd443960a021c807
hidang <yellowstudio.vn@gmail.com>
2021-01-01 14:44:21 +0700

    hoan thanh check chattab xuat hien

--- controllers/addEventMouse.js
+++ controllers/addEventMouse.js
@@ -1,26 +1,29 @@
 console.log('addEventMouse.js');
 const port = chrome.runtime.connect();
 var ChatTabs, I_ChatTabs, Chat_Tabs, I_Chat_Tabs, array_ChatTabName = [], 
-    arrayChatTabName = [], flag = false, ChatTabNames;
+    arrayChatTabName = [], flag = true, ChatTabNames;
 //---------------------------------------------------------------------------------------
+
 window.setInterval(function(){ // Set interval for checking 😑😐
+  //console.log('ne!')
   I_ChatTabs = document.querySelectorAll('[data-testid="messenger-chat-title-text"]');
-  if (I_ChatTabs.length === 0) {
-    if(flag){
-      //console.log('just one');
+  if (I_ChatTabs.length === 0) {//TH: khi các chattab được đóng hết phải reset các giá trị lại
+    if(flag){// length === 0 phải reset các giá trị.. 
+      console.log('just one');
       localStorage.setItem('Chat_Tabs', '[]');
       port.postMessage({ChatTabName: '[]'});
     }
     flag = false;
     return;
   }
-  flag = true;
+  flag = true;// set lại flag: các tab vẫn còn
   I_Chat_Tabs = JSON.parse(localStorage.getItem('Chat_Tabs'));
   if (I_Chat_Tabs === "[]" || I_Chat_Tabs === null) {//trường hợp I_ChatTab.length >0 và I_Chat_Tabs chưa được set Item
     //console.log('dayne')
     Add();
     return;
   }
+
   arrayChatTabName = [];
   I_ChatTabs.forEach(element => {
     arrayChatTabName.push(element.textContent);
@@ -44,14 +47,14 @@ window.setInterval(function(){ // Set interval for checking 😑😐
 
 function Add(ChatTabName_New = true) {
   console.log('add() ne');
-  ChatTabs = document.querySelectorAll('[data-pagelet="ChatTab"]');
+  ChatTabs = document.querySelectorAll('[data-pagelet="ChatTab"]');//FIXME: trường hợp 2 tên giống nhau
   array_ChatTabName = [];
   if(ChatTabs !== "[]"){
     ChatTabs.forEach(element => {
-      ChatTabNames = element.querySelectorAll('[data-testid="messenger-chat-title-text"]');
-      array_ChatTabName.push(ChatTabNames[0].textContent);//FIXME: trường hợp 2 tên giống nhau
-      //console.log(ChatTabNames[0].textContent)
-      if (ChatTabName_New === true) {//FIXME: check ton tai addEventListener
+      ChatTabNames = element.querySelector('[data-testid="messenger-chat-title-text"]');
+      array_ChatTabName.push(ChatTabNames.textContent);
+      //TODO: check ton tai addEventListener
+      if (ChatTabName_New === true) {// TH: lần đầu tiên
         element.addEventListener('mouseenter', () => {
           port.postMessage({thaotac: "show"});
         });
@@ -59,7 +62,8 @@ function Add(ChatTabName_New = true) {
           port.postMessage({thaotac: "hide"});
         });
       }else{
-        if (ChatTabNames[0].textContent == ChatTabName_New) {
+        if (ChatTabNames.textContent == ChatTabName_New) {//TH xuất hiện chattab mới khi đang tồn tại chattab cũ
+          //console.log('them ne');
           element.addEventListener('mouseenter', () => {
             port.postMessage({thaotac: "show"});
           });
